432ed047477d2543486bef85d76abdb3b2ccf824
michele-oliveira <micheleoliveiranovais24@gmail.com>
2025-08-28 14:41:15 -0300

    refactor(server): update server.ts to use database client

--- server.ts
+++ server.ts
@@ -1,5 +1,6 @@
 import fastify from "fastify";
-import crypto from "node:crypto";
+import { db } from "./src/database/client";
+import { courses } from "./src/database/schema";
 
 const server = fastify({
   logger: {
@@ -13,51 +14,8 @@ const server = fastify({
   },
 });
 
-const courses = [
-  { id: "1", title: "Curso de Node.js" },
-  { id: "2", title: "Curso de React" },
-  { id: "3", title: "Curso de React Native" },
-];
+server.get("/courses", async (request, reply) => {
+  const result = await db.select().from(courses);
 
-server.get("/courses", () => {
-  return { courses };
-});
-
-server.get("/courses/:id", (request, reply) => {
-  type Params = {
-    id: string;
-  };
-
-  const params = request.params as Params;
-  const courseId = params.id;
-
-  const course = courses.find((course) => course.id === courseId);
-
-  if (course) {
-    return { course };
-  }
-  return reply.status(404).send();
-});
-
-server.post("/courses", (request, reply) => {
-  type Body = {
-    title: string;
-  };
-
-  const courseId = crypto.randomUUID();
-
-  const body = request.body as Body;
-  const courseTitle = body.title;
-
-  if (!courseTitle) {
-    return reply.status(400).send({ message: "Título obrigatório!" });
-  }
-
-  courses.push({ id: courseId, title: courseTitle });
-
-  return reply.status(201).send({ courseId });
-});
-
-server.listen({ port: 3333 }).then(() => {
-  console.log("HTTP server runnning!");
+  return reply.send({ courses });
 });
