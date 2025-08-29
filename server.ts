c82e878869271f31c2f61da3a2b1e4de2075d222
michele-oliveira <micheleoliveiranovais24@gmail.com>
2025-08-29 14:41:48 -0300

    refactor(server): restructure folders and move routes

--- server.ts
+++ server.ts
@@ -3,11 +3,13 @@ import {
   validatorCompiler,
   serializerCompiler,
   type ZodTypeProvider,
+  jsonSchemaTransform,
 } from "fastify-type-provider-zod";
-import { db } from "./src/database/client.ts";
-import { courses } from "./src/database/schema.ts";
-import { eq } from "drizzle-orm";
-import { z } from "zod";
+import { fastifySwagger } from "@fastify/swagger";
+import scalarAPIReference from "@scalar/fastify-api-reference";
+import { createCourseRoute } from "./src/routes/create-courses.ts";
+import { getCourseByIdRoute } from "./src/routes/get-courses-by-id.ts";
+import { getCoursesRoute } from "./src/routes/get-courses.ts";
 
 const server = fastify({
   logger: {
@@ -21,62 +23,28 @@ const server = fastify({
   },
 }).withTypeProvider<ZodTypeProvider>();
 
+if (process.env.NODE_ENV === "development") {
+  server.register(fastifySwagger, {
+    openapi: {
+      info: {
+        title: "Desafio Node.js",
+        version: "1.0.0",
+      },
+    },
+    transform: jsonSchemaTransform,
+  });
+
+  server.register(scalarAPIReference, {
+    routePrefix: "/docs",
+  });
+}
+
 server.setSerializerCompiler(serializerCompiler);
 server.setValidatorCompiler(validatorCompiler);
 
-server.get("/courses", async (request, reply) => {
-  const result = await db
-    .select({
-      id: courses.id,
-      title: courses.title,
-    })
-    .from(courses);
-
-  return reply.send({ courses: result });
-});
-
-server.get("/courses/:id", async (request, reply) => {
-  type Params = {
-    id: string;
-  };
-
-  const params = request.params as Params;
-  const courseId = params.id;
-
-  const result = await db
-    .select()
-    .from(courses)
-    .where(eq(courses.id, courseId));
-
-  if (result.length > 0) {
-    return { course: result[0] };
-  }
-
-  return reply.status(404).send();
-});
-
-server.post(
-  "/courses",
-  {
-    schema: {
-      body: z.object({
-        title: z.string().min(5, "Título precisa ter 5 caracteres"),
-      }),
-    },
-  },
-  async (request, reply) => {
-    const courseTitle = request.body.title;
-
-    const result = await db
-      .insert(courses)
-      .values({
-        title: courseTitle,
-      })
-      .returning();
-
-    return reply.status(201).send({ courseId: result[0].id });
-  }
-);
+server.register(createCourseRoute);
+server.register(getCourseByIdRoute);
+server.register(getCoursesRoute);
 
 server.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running!");
