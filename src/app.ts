376e3ec0c4d03641ce7af7422012e6aab287ef85
michele-oliveira <micheleoliveiranovais24@gmail.com>
2025-09-02 14:50:51 -0300

    refactor(server): move project listen to app folder to prevent port 3333 restarts

--- src/app.ts
+++ src/app.ts
@@ -7,9 +7,9 @@ import {
 } from "fastify-type-provider-zod";
 import { fastifySwagger } from "@fastify/swagger";
 import scalarAPIReference from "@scalar/fastify-api-reference";
-import { createCourseRoute } from "./src/routes/create-courses.ts";
-import { getCourseByIdRoute } from "./src/routes/get-courses-by-id.ts";
-import { getCoursesRoute } from "./src/routes/get-courses.ts";
+import { createCourseRoute } from "./routes/create-courses.ts";
+import { getCourseByIdRoute } from "./routes/get-courses-by-id.ts";
+import { getCoursesRoute } from "./routes/get-courses.ts";
 
 const server = fastify({
   logger: {
@@ -46,6 +46,4 @@ server.register(createCourseRoute);
 server.register(getCourseByIdRoute);
 server.register(getCoursesRoute);
 
-server.listen({ port: 3333 }).then(() => {
-  console.log("HTTP server running!");
-});
+export { server };
