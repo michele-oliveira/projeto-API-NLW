5d91e0dd9fd36b87da05481beddbc75e24d3090b
michele-oliveira <micheleoliveiranovais24@gmail.com>
2025-09-03 11:08:05 -0300

    feat(courses): update create-course and get-course endpoints

--- src/routes/create-course.test.ts
+++ src/routes/create-course.test.ts
@@ -3,7 +3,7 @@ import request from "supertest";
 import { server } from "../app.ts";
 import { faker } from "@faker-js/faker";
 
-test("cria um curso com sucesso", async () => {
+test("create a course", async () => {
   await server.ready();
 
   const response = await request(server.server)
@@ -11,8 +11,8 @@ test("cria um curso com sucesso", async () => {
     .set("Content-Type", "application/json")
     .send({ title: faker.lorem.words(4) });
 
-  expect(response.status).toEqual(201)
+  expect(response.status).toEqual(201);
   expect(response.body).toEqual({
     courseId: expect.any(String),
-  })
+  });
 });
---
+++ src/routes/get-course-by-id.test.ts
@@ -0,0 +1,22 @@
+import { test, expect } from "vitest";
+import request from "supertest";
+import { server } from "../app.ts";
+import { faker } from "@faker-js/faker";
+import { makeCourse } from "../tests/factories/make-course.ts";
+
+test("get a course by id", async () => {
+  await server.ready();
+
+  const course = await makeCourse();
+
+  const response = await request(server.server).get(`/courses/${course.id}`);
+
+  expect(response.status).toEqual(200);
+  expect(response.body).toEqual({
+    course: {
+      id: expect.any(String),
+      title: expect.any(String),
+      description: null,
+    },
+  });
+});
